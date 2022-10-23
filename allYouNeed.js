8a83a31dcb23ab73c876c224cce49aa985ca8f70
chonuk <jahr2080@gmail.com>
2022-10-23 17:39:35 -0500

    Update allYouNeed.js

--- allYouNeed.js
+++ allYouNeed.js
@@ -1,2 +1,4 @@
 //just copy paste in browser, adjust milliseconds at the end if it's swiping too fast
 setInterval(() => {document.getElementsByClassName('Bgc($c-like-green):a')[0].click(), 1500})
+
+setInterval(() => {document.getElementsByClassName('Bgi($g-ds-background-like):a')[0].click(), 1500})
